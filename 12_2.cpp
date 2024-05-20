0487340227673800c33b3b426c5306b160247503
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-05-20 13:19:01 +0800

    12_2.cpp

--- 12_2.cpp
+++ 12_2.cpp
@@ -1,41 +1,49 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
+#include <cctype>
+#include <cstdlib>
 #include "Queue.h"
 using namespace std;
 
-int main() {
-    Queue<char> queue;
-    char input[12];
-    cin >> input;
-    for (int i = 0; i < 12; i++) {
-        queue.push(input[i]);
-    }
-    while (queue.getSize() > 1) {
-        char a = queue.peek();
-        queue.pop();
-        char op = queue.peek();
-        queue.pop();
-        char b = queue.peek();
-        queue.pop();
-        int result = 0;
-        switch (op) {
-            case '+':
-                result = (a - '0') + (b - '0');
-                break;
-            case '-':
-                result = (a - '0') - (b - '0');
-                break;
-            case '*':
-                result = (a - '0') * (b - '0');
-                break;
-            case '/':
-                result = (a - '0') / (b - '0');
-                break;
+int main()
+{
+    string expression;
+    cin >> expression;
+
+    Queue<int> queue;
+    for (int i = 0; i < expression.length(); i += 3)
+    {
+        int num1 = expression[i] - '0';
+        int num2 = expression[i + 2] - '0';
+        char op = expression[i + 1];
+        int result;
+        switch (op)
+        {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
         }
+        queue.push(result);
         cout << result << endl;
-        queue.push(result + '0');
     }
-    cout << "Sum of the queue: " << queue.peek() - '0' << endl;
+
+    int sum = 0;
+    while (!queue.empty())
+    {
+        sum += queue.peek();
+        queue.pop();
+    }
+    cout << "Sum of the queue: " << sum << endl;
+
     return 0;
 }
 
@@ -44,15 +52,17 @@ int main() {
 #define QUEUE_H
 
 template <typename T>
-class Queue {
+class Queue
+{
 public:
     Queue();
     ~Queue();
     bool empty() const;
     T peek() const;
-    void push(T value);
+    void push(const T& element);
     void pop();
     int getSize() const;
+
 private:
     T* elements;
     int size;
@@ -63,59 +73,65 @@ private:
 };
 
 template <typename T>
-Queue<T>::Queue() {
-    capacity = 16;
+Queue<T>::Queue() : size(0), capacity(1), front(0), back(0)
+{
     elements = new T[capacity];
-    size = 0;
-    front = 0;
-    back = 0;
 }
 
 template <typename T>
-Queue<T>::~Queue() {
+Queue<T>::~Queue()
+{
     delete[] elements;
 }
 
 template <typename T>
-bool Queue<T>::empty() const {
+bool Queue<T>::empty() const
+{
     return size == 0;
 }
 
 template <typename T>
-T Queue<T>::peek() const {
+T Queue<T>::peek() const
+{
     return elements[front];
 }
 
 template <typename T>
-void Queue<T>::push(T value) {
+void Queue<T>::push(const T& element)
+{
     ensureCapacity();
-    elements[back] = value;
+    elements[back] = element;
     back = (back + 1) % capacity;
-    size++;
+    ++size;
 }
 
 template <typename T>
-void Queue<T>::pop() {
+void Queue<T>::pop()
+{
     front = (front + 1) % capacity;
-    size--;
+    --size;
 }
 
 template <typename T>
-int Queue<T>::getSize() const {
+int Queue<T>::getSize() const
+{
     return size;
 }
 
 template <typename T>
-void Queue<T>::ensureCapacity() {
-    if (size >= capacity) {
+void Queue<T>::ensureCapacity()
+{
+    if (size == capacity)
+    {
         T* old = elements;
-        elements = new T[2 * capacity];
-        for (int i = 0; i < size; i++) {
-            elements[i] = old[(front + i) % capacity];
+        capacity *= 2;
+        elements = new T[capacity];
+        for (int i = 0; i < size; ++i)
+        {
+            elements[i] = old[(front + i) % size];
         }
         front = 0;
         back = size;
-        capacity *= 2;
         delete[] old;
     }
 }
