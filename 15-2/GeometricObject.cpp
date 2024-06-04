f08f675bffae92a9749c788ef9fc033c6737c222
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:16:48 +0800

    Update GeometricObject.cpp

--- 15-2/GeometricObject.cpp
+++ 15-2/GeometricObject.cpp
@@ -1,1 +1,17 @@
+#include "GeometricObject.h"
 
+GeometricObject::GeometricObject() {
+    color = " ";
+}
+
+GeometricObject::GeometricObject(string color) {
+    this->color = color;
+}
+
+void GeometricObject::setColor(string color) {
+    this->color = color;
+}
+
+string GeometricObject::getColor() const {
+    return color;
+}
