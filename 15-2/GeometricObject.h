059428311e4e3d58732f4283ff3b1dcfd4a49087
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:16:33 +0800

    Update GeometricObject.h

--- 15-2/GeometricObject.h
+++ 15-2/GeometricObject.h
@@ -1,1 +1,20 @@
+#ifndef GEOMETRICOBJECT_H
+#define GEOMETRICOBJECT_H
 
+#include <string>
+using namespace std;
+
+class GeometricObject {
+private:
+    string color;
+
+public:
+    GeometricObject();
+    GeometricObject(string color);
+    void setColor(string color);
+    string getColor() const;
+    virtual double getVolume() const = 0;
+    virtual double getArea() const = 0;
+};
+
+#endif 
