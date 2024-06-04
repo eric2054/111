5d4c842b563fe08fa040e833823e7fa014214a69
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:12:56 +0800

    Update main.cpp

--- 15-2/main.cpp
+++ 15-2/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include "Ball.h"
+#include "Cube.h"
+#include "GeometricObject.h"
+#include<iomanip>
+using namespace std;
 
+void display(GeometricObject &dis){
+    cout<<fixed<<setprecision(1)<<"area:"<<dis.getArea()<<", "<<"volume:"<<dis.getVolume()<<", "<<"color:"<<dis.getColor()<<endl;
+}
+
+
+int main() {
+    double radius, length;
+    string color;
+    
+    cin>>radius>>color;
+    Ball ball(radius, color);
+
+    
+    cin>>length>>color;
+    Cube cube(length, color);
+
+    
+    cout <<"Ball"<<endl;
+    cout<<fixed<<setprecision(1)<<"radius:"<<ball.getRadius()<<", ";
+    display(ball);
+    
+    cout << "Cube"<<endl;
+    cout<<fixed<<setprecision(1)<<"length:"<<cube.getLength()<<", ";
+    display(cube);
+}
