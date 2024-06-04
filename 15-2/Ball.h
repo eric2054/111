6ee4a675d4474c462553c2e4f55f2f7d8978dea4
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:15:40 +0800

    Update Ball.h

--- 15-2/Ball.h
+++ 15-2/Ball.h
@@ -1,1 +1,19 @@
+#ifndef BALL_H
+#define BALL_H
 
+#include "GeometricObject.h"
+
+class Ball : public GeometricObject {
+private:
+    double radius;
+
+public:
+    Ball();
+    Ball(double radius, string color);
+    void setRadius(double radius);
+    double getRadius() const;
+    double getVolume() const override;
+    double getArea() const override;
+};
+
+#endif 
