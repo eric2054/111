80dfdd1ff0eb6e3802f064516d946ee3faff10cc
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:15:31 +0800

    Update Ball.cpp

--- 15-2/Ball.cpp
+++ 15-2/Ball.cpp
@@ -1,1 +1,26 @@
+#include "Ball.h"
+#include <cmath>
 
+Ball::Ball() {
+    radius = 0; 
+}
+
+Ball::Ball(double radius, string color) : GeometricObject(color) {
+    this->radius = radius;
+}
+
+void Ball::setRadius(double radius) {
+    this->radius = radius;
+}
+
+double Ball::getRadius() const {
+    return radius;
+}
+
+double Ball::getVolume() const {
+    return (4.0 / 3.0) * M_PI * pow(radius, 3);
+}
+
+double Ball::getArea() const {
+    return 4.0 * M_PI * pow(radius, 2);
+}
