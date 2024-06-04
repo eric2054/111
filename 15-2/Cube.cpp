3ef2a0d66aa4185666d983d08f7596d51ac2799f
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:15:57 +0800

    Update Cube.cpp

--- 15-2/Cube.cpp
+++ 15-2/Cube.cpp
@@ -1,1 +1,25 @@
+#include "Cube.h"
+#include <cmath>
+Cube::Cube() {
+    length = 0; 
+}
 
+Cube::Cube(double length, string color) : GeometricObject(color) {
+    this->length = length;
+}
+
+void Cube::setLength(double length) {
+    this->length = length;
+}
+
+double Cube::getLength() const {
+    return length;
+}
+
+double Cube::getVolume() const {
+    return pow(length, 3);
+}
+
+double Cube::getArea() const {
+    return 6 * pow(length,2);
+}
