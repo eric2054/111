4c31cd35e03fddb4a08be2133d811a6ec4ee28a4
eric2054 <90565230+eric2054@users.noreply.github.com>
2024-06-04 16:16:11 +0800

    Update Cube.h

--- 15-2/Cube.h
+++ 15-2/Cube.h
@@ -1,1 +1,20 @@
+#ifndef CUBE_H
+#define CUBE_H
+
+#include "GeometricObject.h"
+
+class Cube : public GeometricObject {
+private:
+    double length;
+
+public:
+    Cube();
+    Cube(double length, string color);
+    void setLength(double length);
+    double getLength() const;
+    double getVolume() const override;
+    double getArea() const override;
+};
+
+#endif
 
